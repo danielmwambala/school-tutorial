26675e05db271ca8fcee270a6533936c6b8b9785
danielmwambala <danielmwambala@gmail.com>
2024-07-25 08:15:21 +0300

    Initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+# Actions / methods go here
+# example
+    def index
+        # handle requests from routes
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # welcome page route
+  # get 'welcome/index'
+  # set welcome page as the root
+  root 'welcome#index'
+end
